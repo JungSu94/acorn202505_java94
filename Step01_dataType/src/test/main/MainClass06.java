c4d0b4f2e4498aaae4c994bf46102e60b95b8634
anjungsu <wjdtn220@naver.com>
2025-06-17 16:57:30 +0900

    Step02_Operator 프로젝트 추가함

---
+++ Step01_dataType/src/test/main/MainClass06.java
@@ -0,0 +1,16 @@
+package test.main;
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		
+	
+	//String type 의 기능(메소드) 사용해 보기
+	
+	String str="abcd1234";
+	//문자열의 길이를 리턴하는 length() 메소드 사용해 보기
+	int length=str.length();
+	//문자열을 대문자로 변경해서 리턴하는 메소드 사용
+	String upper=str.toUpperCase();
+	boolean contains=str.contains("cd");
+	}
+}
