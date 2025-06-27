79a802e5f40c2b1c38739234214b0d872d2acd7f
anjungsu <wjdtn220@naver.com>
2025-06-27 10:59:25 +0900

    Step10_GenericClass 프로젝트 추가함

---
+++ Step10_GenericClass/src/test/box/BananaBox.java
@@ -0,0 +1,17 @@
+package test.box;
+
+import test.fruit.Banana;
+
+public class BananaBox {
+	private Banana item;
+	
+
+	public void pack(Banana item) {
+		this.item=item;
+	}
+
+
+	public Banana unpack() {
+		return item;
+	}
+}
